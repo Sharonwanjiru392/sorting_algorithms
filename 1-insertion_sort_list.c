0fc3b9a6699601d79c99db45ac4ae4dfab13a4c5
Sharon <Sharonwanjiru392@gmail.com>
2023-07-22 07:21:24 -0700

    print_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -34,12 +34,13 @@ void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *iter, *insert, *tmp;
-		if (list == NULL || *list == NULL || (*list)->next == NULL)
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
 	for (iter = (*list)->next; iter != NULL; iter = tmp)
 	{
-		tmp = iter->next;
+				tmp = iter->next;
 		insert = iter->prev;
 
 		while (insert != NULL && iter->n < insert->n)
--- print_list.c
+++ print_list.c
@@ -2,11 +2,11 @@
 #include "sort.h"
 
 /**
-<<<<<<< HEAD
  * print_list - prints a list of integers
  *
  * @list: The to be printed
  */
+void print_list(const listint_t *list)
 {
         int i;
         while (list)
@@ -18,30 +18,4 @@
                 list = list->next;
         }
         printf("\n");
-=======
- * print_list - Prints the elements of a linked list.
- *
- * @list: Pointer to the head of the linked list.
- *
- * Description:
- * This function takes the head of a linked list and prints its elements.
- *
- * Return: Void
- */
-
-void print_list(const listint_t *list)
-{
-	int i;
-
-	i = 0;
-	while (list)
-	{
-		if (i > 0)
-			printf(", ");
-		printf("%d", list->n);
-		++i;
-		list = list->next;
-	}
-	printf("\n");
->>>>>>> Master
 }
