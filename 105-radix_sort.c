605ac1371aed87f33ec7a24f083c4211d017722c
Listus97 <namurendecallistus@gmail.com>
2023-07-24 23:27:49 +0000

    0x1B. C - Sorting algorithms & Big O

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -80,3 +80,4 @@ void radix_sort(int *array, size_t size)
 	}
 
 	free(buff);
+}
--- print_array.c
+++ print_array.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -16,38 +15,9 @@ void print_array(const int *array, size_t size)
         while (array && i < size)
         {
                 if (i > 0)
-                        printf(", ");
-                printf("%d", array[i]);
-                ++i;
-        }
-        printf("\n");
-=======
-nclude <stdlib.h>
-#include <stdio.h>
-
-/**
- * print_array - Prints the elements of an array of integers.
- * @array: Pointer to the array of integers to be printed.
- * @size: The number of elements in the array.
- *
- * Description:
- * This function takes an array of integers and prints its elements.
- * 
- * Return: Void
- */
-
-void print_array(const int *array, size_t size)
-{
-	size_t i;
-
-	i = 0;
-	while (array && i < size)
-	{
-		if (i > 0)
 			printf(", ");
 		printf("%d", array[i]);
 		++i;
 	}
 	printf("\n");
->>>>>>> Master
-}
+}       
