235035fc9624c6f07ff13505187b31a6bd98091e
Sharon <Sharonwanjiru392@gmail.com>
2023-07-26 02:50:19 -0700

    1000-sort_deck.c

--- 1000-sort_deck.c
+++ 1000-sort_deck.c
@@ -1,188 +1,96 @@
-#include "deck.h"
+/*
+ * File: 106-bitonic_sort.c
+ * Auth: Brennan D Baraban
+ */
 
-int _strcmp (const char *s1, const char *s2);
-char get_value(deck_node_t *card);
-void insertion_sort_deck_kind(deck_node_t **deck);
-void insertion_sort_deck_value(deck_node_t **deck);
-void sort_deck(deck_node_t **deck);
+#include "sort.h"
+
+void swap_ints(int *a, int *b);
+void bitonic_merge(int *array, size_t size, size_t start, size_t seq,
+		char flow);
+void bitonic_seq(int *array, size_t size, size_t start, size_t seq, char flow);
+void bitonic_sort(int *array, size_t size);
 
 /**
- * _strcmp - Compares two strings.
- * @s1: The first string to be compared.
- * @s2: The second string to be compared.
+ * swap_ints - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * bitonic_merge - Sort a bitonic sequence inside an array of integers.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ * @start: The starting index of the sequence in array to sort.
+ * @seq: The size of the sequence to sort.
+ * @flow: The direction to sort in.
+ */
+void bitonic_merge(int *array, size_t size, size_t start, size_t seq,
+		char flow)
+{
+	size_t i, jump = seq / 2;
+
+	if (seq > 1)
+	{
+		for (i = start; i < start + jump; i++)
+		{
+			if ((flow == UP && array[i] > array[i + jump]) ||
+			    (flow == DOWN && array[i] < array[i + jump]))
+				swap_ints(array + i, array + i + jump);
+		}
+		bitonic_merge(array, size, start, jump, flow);
+		bitonic_merge(array, size, start + jump, jump, flow);
+	}
+}
+
+/**
+ * bitonic_seq - Convert an array of integers into a bitonic sequence.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ * @start: The starting index of a block of the building bitonic sequence.
+ * @seq: The size of a block of the building bitonic sequence.
+ * @flow: The direction to sort the bitonic sequence block in.
+ */
+void bitonic_seq(int *array, size_t size, size_t start, size_t seq, char flow)
+{
+	size_t cut = seq / 2;
+	char *str = (flow == UP) ? "UP" : "DOWN";
+
+	if (seq > 1)
+	{
+		printf("Merging [%lu/%lu] (%s):\n", seq, size, str);
+		print_array(array + start, seq);
+
+		bitonic_seq(array, size, start, cut, UP);
+		bitonic_seq(array, size, start + cut, cut, DOWN);
+		bitonic_merge(array, size, start, seq, flow);
+
+		printf("Result [%lu/%lu] (%s):\n", seq, size, str);
+		print_array(array + start, seq);
+	}
+}
+
+/**
+ * bitonic_sort - Sort an array of integers in ascending
+ *                order using the bitonic sort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
  *
- * Return: Positive byte difference if s1 > s2
- *         0 if s1 == s2
- *         Negative byte difference if s1 < s2
+ * Description: Prints the array after each swap. Only works for
+ * size = 2^k where k >= 0 (ie. size equal to powers of 2).
  */
-int _strcmp(const char *s1, const char *s2)
+void bitonic_sort(int *array, size_t size)
 {
-        /* Compare characters until a difference is found or the end of a string is reached */
-        while (*s1 && *s2 && *s1 == *s2)
-        {
-                 /* Move to the next character in s1 */
-                s1++;
-                /* Move to the next character in s2 */
-                s2++;
-        }
+	if (array == NULL || size < 2)
+		return;
 
-        if (*s1 != *s2)
-                /* Return the difference between the corresponding characters in s1 and s2 */
-                return (*s1 - *s2);
-         /* If s1 and s2 are equal, return 0 */
-        return (0);
-}
-
-/**
- * get_value - Get the numerical value of a card.
- * @card: A pointer to a deck_node_t card.
- *
- * Return: The numerical value of the card.
- */
-char get_value(deck_node_t *card)
-{
-        if (_strcmp(card->card->value, "Ace") == 0)
-                return (0);  /* If the card value is "Ace", return 0 */
-
-        if (_strcmp(card->card->value, "1") == 0)
-                return (1);  /* If the card value is "1", return 1 */
-
-        if (_strcmp(card->card->value, "2") == 0)
-                return (2);  /* If the card value is "2", return 2 */
-
-        if (_strcmp(card->card->value, "3") == 0)
-                return (3);  /* If the card value is "3", return 3 */
-
-        if (_strcmp(card->card->value, "4") == 0)
-                return (4);  /* If the card value is "4", return 4 */
-
-        if (_strcmp(card->card->value, "5") == 0)
-                return (5);  /* If the card value is "5", return 5 */
-
-        if (_strcmp(card->card->value, "6") == 0)
-                return (6);  /* If the card value is "6", return 6 */
-
-        if (_strcmp(card->card->value, "7") == 0)
-                return (7);  /* If the card value is "7", return 7 */
-
-        if (_strcmp(card->card->value, "8") == 0)
-                return (8);  /* If the card value is "8", return 8 */
-
-        if (_strcmp(card->card->value, "9") == 0)
-                return (9);  /* If the card value is "9", return 9 */
-
-        if (_strcmp(card->card->value, "10") == 0)
-                return (10); /* If the card value is "10", return 10 */
-
-        if (_strcmp(card->card->value, "Jack") == 0)
-                return (11); /* If the card value is "Jack", return 11 */
-
-        if (_strcmp(card->card->value, "Queen") == 0)
-                return (12); /* If the card value is "Queen", return 12 */
-
-        return (13);    /* If the card value is not matched, return 13 */
-}
-
-
-/**
- * insertion_sort_deck_kind - Sort a deck of cards from spades to diamonds.
- * @deck: A pointer to the head of a deck_node_t doubly-linked list.
- */
-void insertion_sort_deck_kind(deck_node_t **deck)
-{
-        deck_node_t *iter, *insert, *tmp;
-
-        for (iter = (*deck)->next; iter != NULL; iter = tmp)
-        {
-                tmp = iter->next;
-                insert = iter->prev;
-                /* Move the current card towards the beginning of the suit until it is in the correct position */
-                while (insert != NULL && insert->card->kind > iter->card->kind)
-                {
-                        /* Set the next node of insert to the next node of iter */
-                        insert->next = iter->next;
-                        if (iter->next != NULL)
-                                /* If iter has a next node, set its previous node to insert */
-                                iter->next->prev = insert;
-                         /* Set the previous node of iter to the previous node of insert */
-                        iter->prev = insert->prev;
-                        /* Set the next node of iter to insert */
-                        iter->next = insert;
-
-                        if (insert->prev != NULL)
-                                /* If insert has a previous node, set its next node to iter */
-                                insert->prev->next = iter;
-                        else
-                                /* If insert is the first node, update the deck head to iter */
-                                *deck = iter;
-                        /* Set the previous node of insert to iter */
-                        insert->prev = iter;
-                        /* Update insert to the previous node */
-                        insert = iter->prev;
-                }
-        }
-}
-
-/**
- * insertion_sort_deck_value - Sort a deck of cards sorted from
- *                             spades to diamonds from ace to king.
- * @deck: A pointer to the head of a deck_node_t doubly-linked list.
- */
-void insertion_sort_deck_value(deck_node_t **deck)
-{
-        deck_node_t *iter, *insert, *tmp;
-
-        /* Iterate through the deck starting from the second card */
-        for (iter = (*deck)->next; iter != NULL; iter = tmp)
-        {
-                /* Store the next node before potential reordering */
-                tmp = iter->next;
-                /* Start with the preceding node of the current node */
-                insert = iter->prev;
-
-                /* Compare the kind and value of cards to determine the correct position */
-                while (insert != NULL &&
-                       insert->card->kind == iter->card->kind &&
-                       get_value(insert) > get_value(iter))
-                {
-                         /* Adjust the next pointer of the preceding node */
-                        insert->next = iter->next;
-                        if (iter->next != NULL)
-                                /* Adjust the previous pointer of the following node */
-                                iter->next->prev = insert;
-                        /* Set the previous pointer of the current node to the previous of the preceding node */
-                        iter->prev = insert->prev;
-                        /* Set the next pointer of the current node to the preceding node */
-                        iter->next = insert;
-
-                        if (insert->prev != NULL)
-                                /* Adjust the next pointer of the preceding node */
-                                insert->prev->next = iter;
-                        else
-                                 /* Update the head of the deck if the preceding node is NULL (indicating it is the new head) */
-                                *deck = iter;
-                         /* Set the previous pointer of the preceding node to the current node */
-                        insert->prev = iter;
-                        /* Move to the previous node */
-                        insert = iter->prev;
-                }
-        }
-}
-
-/**
- * sort_deck - Sort a deck of cards from ace to king and
- *             from spades to diamonds.
- * @deck: A pointer to the head of a deck_node_t doubly-linked list.
- */
-void sort_deck(deck_node_t **deck)
-{
-        /* Check if the deck is empty or has only one card */
-        if (deck == NULL || *deck == NULL || (*deck)->next == NULL)
-                return;
-
-        /* Sort the deck based on card kind (spades to diamonds) */
-        insertion_sort_deck_kind(deck);
-
-        /* Sort the deck based on card value (ace to king) within each kind */
-        insertion_sort_deck_value(deck);
+	bitonic_seq(array, size, 0, size, UP);
 }
